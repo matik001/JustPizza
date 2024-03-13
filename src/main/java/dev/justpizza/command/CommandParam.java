25a2a5682eed24724e1634075b2c394b968094f2
marwar22 <amarcinw@gmail.com>
2024-03-13 08:40:25 +0100

    Marked unused classes as unused

--- src/main/java/dev/justpizza/command/CommandParam.java
+++ src/main/java/dev/justpizza/command/CommandParam.java
@@ -1,5 +1,6 @@
 package dev.justpizza.command;
 
+/** This class is currently unused **/
 public class CommandParam{
     public CommandParamSchema schema;
     public String paramValue; /// i.e. AlaHasACat for --name AlaHasACat
