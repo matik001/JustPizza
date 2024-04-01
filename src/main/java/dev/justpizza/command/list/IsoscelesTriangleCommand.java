6257ebfb3174f03d27f1f7e53f8568af4d6edbb7
Marcin Wróbel <amarcinw@gmail.com>
2024-04-02 00:25:26 +0200

    Fixed IsoscelesTriangle math

--- src/main/java/dev/justpizza/command/list/IsoscelesTriangleCommand.java
+++ src/main/java/dev/justpizza/command/list/IsoscelesTriangleCommand.java
@@ -2,7 +2,7 @@ package dev.justpizza.command.list;
 
 import dev.justpizza.argparser.ArgParser;
 import dev.justpizza.command.abstractList.CreateShapeCommand;
-import dev.justpizza.shape.EquilateralTriangle;
+import dev.justpizza.shape.IllegalShapeException;
 import dev.justpizza.shape.IsoscelesTriangle;
 import dev.justpizza.shape.Shape;
 
@@ -11,10 +11,12 @@ import java.util.Objects;
 
 public class IsoscelesTriangleCommand extends CreateShapeCommand {
     public static final String name = "isotriangle";
-public static final String description = "calculates isosceles triangle characteristics";
+    public static final String description = "calculates isosceles triangle characteristics";
+
     public IsoscelesTriangleCommand() {
         super(name, description);
     }
+
     @Override
     protected void initArgParser(ArgParser argParser) {
 
@@ -27,29 +29,32 @@ public static final String description = "calculates isosceles triangle characte
         var options = List.of("base", "side", "height", "area");
         IsoscelesTriangle triangle = null;
         for (int i = 0; i < options.size(); i++) {
-            for (int j = i+1; j < options.size(); j++) {
+            for (int j = i + 1; j < options.size(); j++) {
                 var key1 = options.get(i);
                 var key2 = options.get(j);
                 var val1 = argParser.argValues.get(key1);
                 var val2 = argParser.argValues.get(key2);
-                if(val1 == null || val2 == null)
+                if (val1 == null || val2 == null)
                     continue;
-                if(Objects.equals(key1, "base") && Objects.equals(key2, "side"))
-                    triangle = IsoscelesTriangle.fromBaseSide(val1, val2);
-                if(Objects.equals(key1, "base") && Objects.equals(key2, "height"))
-                    triangle = IsoscelesTriangle.fromBaseHeight(val1, val2);
-                if(Objects.equals(key1, "base") && Objects.equals(key2, "area"))
-                    triangle = IsoscelesTriangle.fromBaseArea(val1, val2);
-                if(Objects.equals(key1, "side") && Objects.equals(key2, "height"))
-                    triangle = IsoscelesTriangle.fromHeightSide(val2, val1);
-                if(Objects.equals(key1, "side") && Objects.equals(key2, "area"))
-                    triangle = IsoscelesTriangle.fromSideArea(val1, val2);
-                if(Objects.equals(key1, "height") && Objects.equals(key2, "area"))
-                    triangle = IsoscelesTriangle.fromHeightArea(val1, val2);
+                try {
+                    if (Objects.equals(key1, "base") && Objects.equals(key2, "side"))
+                        triangle = IsoscelesTriangle.fromBaseSide(val1, val2);
+                    if (Objects.equals(key1, "base") && Objects.equals(key2, "height"))
+                        triangle = IsoscelesTriangle.fromBaseHeight(val1, val2);
+                    if (Objects.equals(key1, "base") && Objects.equals(key2, "area"))
+                        triangle = IsoscelesTriangle.fromBaseArea(val1, val2);
+                    if (Objects.equals(key1, "side") && Objects.equals(key2, "height"))
+                        triangle = IsoscelesTriangle.fromHeightSide(val2, val1);
+                    if (Objects.equals(key1, "side") && Objects.equals(key2, "area"))
+                        triangle = IsoscelesTriangle.fromSideArea(val1, val2);
+                    if (Objects.equals(key1, "height") && Objects.equals(key2, "area"))
+                        triangle = IsoscelesTriangle.fromHeightArea(val1, val2);
+                } catch (IllegalShapeException e) {
+                    System.out.println(e.getMessage());
+                    return null;
+                }
             }
         }
         return triangle;
     }
-
-
 }
--- src/main/java/dev/justpizza/shape/IsoscelesTriangle.java
+++ src/main/java/dev/justpizza/shape/IsoscelesTriangle.java
@@ -13,7 +13,8 @@ public class IsoscelesTriangle extends Shape {
         return new IsoscelesTriangle(base, height);
     }
 
-    public static IsoscelesTriangle fromBaseSide(double base, double side) {
+    public static IsoscelesTriangle fromBaseSide(double base, double side) throws IllegalShapeException {
+        if (2 * side < base) throw new IllegalShapeException("IsoscelesTriangle with this parameters does not exist");
         double height = Math.sqrt(Math.pow(side, 2) - Math.pow(base / 2, 2));
         return new IsoscelesTriangle(base, height);
     }
@@ -22,24 +23,30 @@ public class IsoscelesTriangle extends Shape {
         double height = 2 * area / base;
         return new IsoscelesTriangle(base, height);
     }
+
     public static IsoscelesTriangle fromHeightArea(double height, double area) {
         double base = 2 * area / height;
         return new IsoscelesTriangle(base, height);
     }
-    public static IsoscelesTriangle fromHeightSide(double height, double side) {
-        double base = Math.sqrt(Math.pow(side, 2) - Math.pow(height, 2));
+
+    public static IsoscelesTriangle fromHeightSide(double height, double side) throws IllegalShapeException {
+        if (side < height) throw new IllegalShapeException("IsoscelesTriangle with this parameters does not exist");
+        double base = Math.sqrt(Math.pow(side, 2) - Math.pow(height, 2)) * 2;
         return new IsoscelesTriangle(base, height);
     }
 
-    public static IsoscelesTriangle fromSideArea(double side, double area) {
-        double height = 2 * area / side;
-        double base = Math.sqrt(4 * Math.pow(height, 2) + Math.pow(side, 2));
-        return new IsoscelesTriangle(base, height);
+    public static IsoscelesTriangle fromSideArea(double side, double area) throws IllegalShapeException {
+        // height^4 - height^2 * side^2 + area^2 = 0
+        double delta = Math.pow(side, 4) - 4 * Math.pow(area, 2);
+        if (delta < 0) throw new IllegalShapeException("IsoscelesTriangle with this parameters does not exist");
+        double height = Math.sqrt((Math.pow(side, 2) + Math.sqrt(delta)) / 2);
+        return IsoscelesTriangle.fromHeightSide(height, side);
     }
 
     public double getBase() {
         return base;
     }
+
     public double getSide() {
         return Math.sqrt(Math.pow(base / 2, 2) + Math.pow(height, 2));
     }
@@ -49,12 +56,13 @@ public class IsoscelesTriangle extends Shape {
     }
 
     public double getArea() {
-        return base*height/2;
+        return base * height / 2;
     }
 
     @Override
     public void printCharacteristic() {
-        System.out.println(STR. "Triangle characteristics:\n\tbase: \{ getBase() } \n\tside: \{ getSide() }\n\theight: \{ getHeight() }\n\tarea: \{ getArea() }\n" );
+        System.out.println(
+                STR. "IsoscelesTriangle characteristics:\n\tbase: \{ getBase() } \n\tside: \{ getSide() }\n\theight: \{ getHeight() }\n\tarea: \{ getArea() }\n" );
     }
 
     @Override
