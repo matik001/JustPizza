9f0a47fe79f5fd7faea4556bacba11c80e6de54e
Jacek <ja.d@wp.pl>
2024-04-20 15:46:36 +0200

    Added fields, construcotor and methods (yet to be implemented) to DoubleFigureCommand

--- src/main/java/dev/justpizza/command/list/DoubleFigureCommand.java
+++ src/main/java/dev/justpizza/command/list/DoubleFigureCommand.java
@@ -1,4 +1,25 @@
 package dev.justpizza.command.list;
 
-public class DoubleFigureCommand {
+import dev.justpizza.argparser.ArgParser;
+import dev.justpizza.command.abstractList.CreateShapeCommand;
+import dev.justpizza.config.AppSettings;
+import dev.justpizza.shape.Shape;
+import dev.justpizza.translations.TranslationKey;
+
+public class DoubleFigureCommand extends CreateShapeCommand {
+    public static final String name = "double";
+    public static final String description =
+            AppSettings.global.translations.get(TranslationKey.circumcircle_description);
+
+    public DoubleFigureCommand(){ super(name, description); }
+
+    @Override
+    protected void initArgParser(ArgParser argParser) {
+        return; //TODO: implement
+    }
+
+    @Override
+    protected Shape createShape(ArgParser argParser) {
+        return null;
+    }
 }
