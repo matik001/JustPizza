e886d12b39949d4528eebee3c7e7ac6c51649e7e
Jacek <ja.d@wp.pl>
2024-04-23 10:08:31 +0200

    Created CircleTest class and added junit dependency

---
+++ src/test/java/dev/justpizza/tests/CircleTest.java
@@ -0,0 +1,40 @@
+package dev.justpizza.tests;
+
+import dev.justpizza.shape.circle.Circle;
+import org.junit.jupiter.api.Test;
+
+class CircleTest {
+
+    //private final Circle circle = Circle.fromRadius(1);
+    @org.junit.jupiter.api.Test
+    void fromRadius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fromArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fromPerimeter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRadius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPerimeter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createCircumcircle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void doubleArea() {
+    }
+}
